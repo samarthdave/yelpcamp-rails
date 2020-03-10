6042c87647aeb0b93033abc6f65a93acc0c3631c
Samarth Dave <samarthdave@users.noreply.github.com>
2020-03-10 13:54:46 -0500

    Not sure but here is what was staged a couple of years back

--- app/controllers/campgrounds_controller.rb
+++ app/controllers/campgrounds_controller.rb
@@ -2,26 +2,26 @@ class CampgroundsController < ApplicationController
 	before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destory]
 	before_action :find_campground, only: [:show, :edit, :update, :destroy]
 	before_action :is_owner, only: [:edit, :update, :destroy]
-    def index
-        @campgrounds = Campground.all.order('created_at DESC')
-    end
-    
-    def show
-        @campground = Campground.find(params[:id])
-        @user = @campground.user
-        @comments = @campground.comments.order("created_at DESC")
-    end
-    
-    def new
-        @campground = Campground.new
-    end
-    
+	def index
+		@campgrounds = Campground.all.order('created_at DESC')
+	end
+	
+	def show
+		@campground = Campground.find(params[:id])
+		@user = @campground.user
+		@comments = @campground.comments.order("created_at DESC")
+	end
+	
+	def new
+		@campground = Campground.new
+	end
+	
 	def create
 		@campground = Campground.new(campground_params)
 		@campground.user = current_user
 		
 		if @campground.save
-       		flash[:success] = "Saved \"#{@campground.name}\"."
+			flash[:success] = "Saved \"#{@campground.name}\"."
 			redirect_to campgrounds_path
 		else
 			flash[:danger] = "Could not save campground."
@@ -31,7 +31,6 @@ class CampgroundsController < ApplicationController
 
 	def edit
 	end
-    
 	def update
 		if @campground.update(campground_params)
 			flash[:success] = "Updated \"#{@campground.name}\"."
@@ -48,24 +47,23 @@ class CampgroundsController < ApplicationController
 		else
 			flash[:danger] = "Could not delete the campground."
 		end
-    	redirect_to campgrounds_path
+		redirect_to campgrounds_path
 	end
   
-    private
-    
+	private
+	
 		def find_campground
 			@campground = Campground.find(params[:id])
 		end
 
 		def campground_params
-           params.require(:campground).permit(:name, :url, :description) 
-        end
-        
-        def is_owner
+			params.require(:campground).permit(:name, :url, :description) 
+		end
+
+		def is_owner
 			if @campground.user != current_user
 				flash[:danger] = "You can only do that with your campgrounds."
 				redirect_to @campground
 			end
-        end
-    
+		end
 end
